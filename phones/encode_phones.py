4147e25227b6b2277c28a76e8e2362d4ba4c252b
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-12-14 14:28:30 +0530

    Phones testing

--- phones/encode_phones.py
+++ phones/encode_phones.py
@@ -18,6 +18,8 @@ malayalam = {}
 
 for l in table:
 	key = l.split('\t')[0]
+	if len(key) > 1:
+		print key
 	value = l.split('\t')[1][:-1]
 	malayalam[key] = value
 
@@ -28,19 +30,19 @@ for k, v in malayalam.iteritems():
 
 extra = []
 
-i=0
-data2 = []
-while i < len(data):
-	c = data[i]
-	c2 = data[i:i+2]
-	if c2 in malayalam and c2 not in data2:
-		data2.append(malayalam)
-		i+=2
-	elif c2 in malayalam:
-		i+=2
-	else:
-		i+=1
-print data2
+# i=0
+# data2 = []
+# while i < len(data):
+# 	c = data[i]
+# 	c2 = data[i:i+2]
+# 	if c2 not in malayalam and c2 not in data2:
+# 		data2.append(malayalam)
+# 		i+=2
+# 	elif c2 in malayalam:
+# 		i+=2
+# 	else:
+# 		i+=1
+# print data2
 
 for c in data:
 	if c not in malayalam and c not in extra:
