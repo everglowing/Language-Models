947fee913056c51ad5052596fa1b602ac834d182
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-12-17 11:16:23 +0530

    Adding common encoding multilingual

--- Common_Encoding_Weights/plot.py
+++ Common_Encoding_Weights/plot.py
@@ -3,7 +3,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy.interpolate import spline
 import numpy as np
 
-def moving_average(a, n=3000) :
+def moving_average(a, n=100) :
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
